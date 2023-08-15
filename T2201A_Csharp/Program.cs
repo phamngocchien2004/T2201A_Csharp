560ebe7178f23f39c639b3027645935fd9d78b8c
phamngocchien2004@@ <114908347+phamngocchien2004@users.noreply.github.com>
2023-08-15 16:25:36 +0700

    Add project files.

---
+++ T2201A_Csharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using T2201A_Csharp.slot5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PhoneBook phoneBook = new PhoneBook();
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("Phone Book Menu:");
+            Console.WriteLine("1. Insert Phone");
+            Console.WriteLine("2. Remove Phone");
+            Console.WriteLine("3. Update Phone");
+            Console.WriteLine("4. Search Phone");
+            Console.WriteLine("5. Sort");
+            Console.WriteLine("6. Exit");
+            Console.Write("Choose an option: ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter phone: ");
+                    string phone = Console.ReadLine();
+                    phoneBook.insertPhone(name, phone);
+                    break;
+                case 2:
+                    Console.Write("Enter name to remove: ");
+                    string nameToRemove = Console.ReadLine();
+                    phoneBook.RemovePhone(nameToRemove);
+                    break;
+                case 3:
+                    Console.Write("Enter name to update: ");
+                    string nameToUpdate = Console.ReadLine();
+                    Console.Write("Enter new phone: ");
+                    string newPhone = Console.ReadLine();
+                    phoneBook.UpdatePhone(nameToUpdate, newPhone);
+                    break;
+                case 4:
+                    Console.Write("Enter name to search: ");
+                    string nameToSearch = Console.ReadLine();
+                    phoneBook.SearchPhone(nameToSearch);
+                    break;
+                case 5:
+                    phoneBook.Sort();
+                    Console.WriteLine("Phone Book sorted.");
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please choose again.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
